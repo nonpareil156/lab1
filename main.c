b9cb13bd3170543ab43748a539a9e743e1f523b8
nonpareil156 <atenabehzadi86@gmail.com>
2025-09-18 13:10:28 +0200

    Update main.c

--- main.c
+++ main.c
@@ -1,7 +1,12 @@
-#include <stdio.h>
+#include <stdio.h>;
 
-int main(){
-printf("HELLO!");
-return 0;
+int main{} {
+    int x;
 
+    printf("Dear user!");
+    printf("Enter a number, I will print its square!");
+    scanf("%d", x);
+    x = x*x;
+    printf("x square is: %f", x);
+    return 0;
 }
